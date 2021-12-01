bf0df7c9b9364be2be735d5e29b1879b49c73029
MarcosF19 <86076983+MarcLipeSL20@users.noreply.github.com>
2021-12-01 00:27:34 -0300

    Ok

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,9 +3,20 @@ function verificar() {
     var ano = data.getFullYear();
     var fano = document.getElementById('txtano');
     var res = document.querySelector('div#res');
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert("[ERRO] Verifique os dados e tente novamente!");
     } else {
-        window.alert("tudo ok!")
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value); 
+        var gênero = ''; 
+        var img = document.createElementNS('img'); 
+        img.setAttribute('id', 'foto');
+        if(fsex[0].checked) {
+            gênero = 'Homem';
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'; 
+        }
+        res.style.textAlign = 'center'; 
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`; 
     }
 }

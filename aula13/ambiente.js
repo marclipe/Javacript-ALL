9f0113adc98837e57841e1b70673aa2d83562d75
MarcosF19 <86076983+MarcLipeSL20@users.noreply.github.com>
2021-12-21 09:58:24 -0300

    Node JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1; 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 10)
+
+/*var c = 1; 
+while(c <= 3){
+    console.log(`Passo ${c}`);
+    c++
+}*
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/

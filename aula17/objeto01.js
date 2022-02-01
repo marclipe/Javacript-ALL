f1d1331b6a851c3f8069b1dd3ff49aebe9089ef5
Marcos Felipe <marcos.lima52@fatec.sp.gov.br>
2022-01-31 21:28:39 -0300

    Js.file

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -6,5 +6,5 @@ engordar(p=0){
     this.peso += p 
 }}
 
-amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+amigo.engordar(2);
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`);

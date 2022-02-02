b453087391a7e77e842fda785b67bb73f6da88ca
Marcos Felipe <marcos.lima52@fatec.sp.gov.br>
2022-02-02 11:17:45 -0300

    Expressions and Operators

---
+++ rockall/Expressões e Operadores/Expre-and-Opera/ex.js
@@ -0,0 +1,14 @@
+/*Expreessões e Operadores
+
+- Expressions
+- Operators
+    Binary 
+    Unary 
+    Ternary
+*/
+
+let number = 1
+
+console.log(false ? 'alo' : 'nada')
+//typeof - operador unário 
+//console.log(typeof number)

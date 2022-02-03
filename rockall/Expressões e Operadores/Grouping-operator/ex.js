b4f3168c25850ac4090db80ee24cd95871f4bf56
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-03 16:04:38 -0300

    Operators

---
+++ rockall/Expressões e Operadores/Grouping-operator/ex.js
@@ -0,0 +1,6 @@
+//Grouping Operator 
+let total = (2 + 3) * 5
+console.log(total)
+
+let letter = ("Alo alo" + "você")
+console.log(letter)
---
+++ rockall/Expressões e Operadores/Opera-Arit/ex.js
@@ -0,0 +1,30 @@
+//Operadores Aritméticos
+
+//multiplicação * 
+console.log(3.2 * 5)
+
+//divisão /
+console.log(12/2)
+
+//soma +
+console.log(34 + 67)
+
+//subtração - 
+console.log(34 - 23)
+
+//resto da divisão %
+let remainder 
+remainder = 11 % 3 
+console.log(remainder)
+
+//incremento ++ 
+let increment = 0
+console.log(++increment)
+console.log(increment)
+
+//decremento -- 
+let decrement = 0 
+console.log(--decrement)
+
+//exponencial **
+console.log(3 ** 3)

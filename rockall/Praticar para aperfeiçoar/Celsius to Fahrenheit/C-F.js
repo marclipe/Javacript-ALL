7204daccf8ad86eaed856957bc045153a140038b
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-17 21:12:46 -0300

    Celsius to Fahrenheit

--- rockall/Praticar para aperfeiçoar/Celsius to Fahrenheit/C-F.js
+++ rockall/Praticar para aperfeiçoar/Celsius to Fahrenheit/C-F.js
@@ -1,18 +1,32 @@
-console.log("Celsius to Fahrenheit")
-//5:36
 //transformDegree('50F')
 function transformDegree(degree) {
     const celsiusExists = degree.toUpperCase().includes('C')
     const fahrenheitExists = degree.toUpperCase().includes('F')
 
-    if(!celsiusExists && fahrenheitExists) {
-        throw new Error('Grau não identificado!')
+    //Fluxo de Erro 
+    if (!celsiusExists && !fahrenheitExists) {
+        throw new Error('Grau não identificado')
+    } 
+
+    //Fluxo ideal, F -> C
+    let updatedDegree = Number(degree.toUpperCase().replace("F", ""));  
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9 
+    let degreeSign = 'C'
+
+    //Fluxo alternativo C -> 
+    if (celsiusExists) {
+        updatedDegree = Number(degree.toUpperCase().replace("C", ""));  
+        formula = celsius => celsius * 9/5 + 32  
+        degreeSign = 'F'    
     }
+
+    return formula(updatedDegree) + degreeSign
 }
 
 try {
-    transformDegree('50F')
+    console.log(transformDegree('10C'))
+    console.log(transformDegree('50F'))
     transformDegree('50Z')
 } catch (error) {
-    console.log(error)
+    console.log(error.message)
 }

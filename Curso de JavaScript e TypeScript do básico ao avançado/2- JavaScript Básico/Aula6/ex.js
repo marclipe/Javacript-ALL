11eef14e9dc49d26c188c7bab0982fb278f44443
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-25 20:02:22 -0300

    Let and Const

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula6/ex.js
@@ -0,0 +1,20 @@
+let nome = 'João'; 
+console.log(nome, "nasceu em 1984."); 
+console.log("Em 2000", nome, "conheceu Maria."); 
+
+//Não podemos criar variáveis com palavras reservadas 
+//Variáveis precisam ter nomes significados 
+//Não podemos começar uma variável com um número 
+//Não podem conter espaços ou traços 
+//Utilizando camelCase - Primeira Letra Min. e segunda Maius
+//Case - sensitive - Letra Maiúscula faz diferença sim
+//Não podemos redeclarar variáveis com let
+//Não utilize VAR, UTILIZE LET
+let name_; 
+name_ = "Qualquer valor"; 
+console.log(nome)
+
+let nomeCliente = 'MarcLipe'; 
+nomeCliente = 'Marcos Felipe';
+console.log(nomeCliente); 
+
---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula7/Constantes com const/ex.js
@@ -0,0 +1,13 @@
+// + - * /  
+//String = Text | Number = Número 
+const primeiroNumero = 5; //Number 
+const segundoNumero = 10; 
+const resultado = primeiroNumero * segundoNumero; 
+const resultadoDuplicado = resultado * 2; 
+let resultadoTriplicado = resultado * 3; 
+resultadoTriplicado = resultadoTriplicado + 5; 
+console.log(resultado); 
+console.log(resultadoDuplicado)
+console.log(resultadoTriplicado); 
+
+console.log(typeof primeiroNumero); 

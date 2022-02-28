90022d2871c37b801df5172ee33e66e9cbb125a8
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-27 22:27:29 -0300

    Tipos primitivos

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula10/Tipos de dados primitivos/index.js
@@ -0,0 +1,29 @@
+//String
+const nome = 'Luiz'; 
+const nome1 = "Luiz";
+const nome2 = `Luiz`;
+
+//Number 
+const num1 = 10;
+const num2 = 10.52; 
+
+//Undefined = Não aponta para local nenhum na memória (só no JS)
+let nomeAluno; 
+
+//Null = nulo -> não aponta para local nenhum na memória, não tem valor 
+const sobrenomeAluno = null; 
+
+//Boolean - true or false - só duas possibilidades de valores  
+const aprovado = true; 
+
+//typeof -> ver o tipo de uma variável 
+console.log(typeof nome); 
+
+let a = 2; 
+const b = a; 
+console.log(a, b); //2, 2
+
+a = 3; 
+console.log(a, b); //3, 2
+
+//Existe o symbol será visto mais a frente 

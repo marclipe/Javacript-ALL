be0e66e24a8babacf4fb7bc0ac7a91a2af2433e9
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-28 20:52:16 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula10/Operadores aritméticos, de atribuição e incremento/index.js
@@ -0,0 +1,30 @@
+//Aritméticos + - / * 
+//Potenciação **
+//Resto da divisão %
+// + Adição / Concatenação 
+const num1 = 10; 
+const num2 = 5; 
+const num3 = 10; 
+console.log((num1 + num2) * num3); 
+
+/*
+Ordem de Precedência 
+()
+**
+/ % 
++ - 
+*/
+
+let contador = 1; 
+console.log(++contador);
+console.log(--contador)
+console.log(contador)
+
+let cont = 2; 
+cont *= 2; //cont = cont * 2
+console.log(cont)
+
+const n1 = Number('10')
+const n2 = parseInt('5')  
+const n3 = parseFloat('5.5')
+console.log(n1 * n2 * n3)

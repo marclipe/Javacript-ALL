a3f0d98bd55fcd881ce8726656393d684347f272
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-02 20:52:44 -0300

    alert, confirm e prompt (Navegador) e Exercicio

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula11/js/sctipt.js
@@ -0,0 +1,7 @@
+//Exercício 
+const numeroUm = parseFloat(prompt('Digite o primeiro número: '));
+const numeroDois = parseFloat(prompt('Digite o segundo número: ')); 
+
+const resultado = numeroUm + numeroDois; 
+alert(`O resultado entre ${numeroUm} e ${numeroDois} é igual a ${resultado}`); 
+

8541e7c2f27dd2a5a6526c2115bce761e6a90ec9
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-03 15:24:33 -0300

    Exercício Soma

--- Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula11/js/sctipt.js
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula11/js/sctipt.js
@@ -1,7 +1,18 @@
 //Exercício 
-const numeroUm = parseFloat(prompt('Digite o primeiro número: '));
+/*const numeroUm = parseFloat(prompt('Digite o primeiro número: '));
 const numeroDois = parseFloat(prompt('Digite o segundo número: ')); 
 
 const resultado = numeroUm + numeroDois; 
-alert(`O resultado entre ${numeroUm} e ${numeroDois} é igual a ${resultado}`); 
+alert(`O resultado entre ${numeroUm} e ${numeroDois} é igual a ${resultado}`); */
+
+/*Feito na aula pelo professor */
+
+let num1 = prompt('Digite um número: '); 
+let num2 = prompt('Digite outro número: '); 
+
+num1 = Number(num1); 
+num2 = Number(num2); 
+
+const resultado = num1 + num2; 
+alert(`O resultado da sua conta foi: ${resultado}`); 
 

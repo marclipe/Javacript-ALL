79568f45a9a44fff7c550b944a2a85aba6390e6e
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-07 23:38:06 -0300

    Exercício de Strings

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula13/aula13.js
@@ -0,0 +1,7 @@
+// 0123456789 
+let umaString = 'O rato roeu a roupa do rei de Roma.'; 
+//console.log(umaString.slice(2, 6)); 
+//console.log(umaString.slice(-5, -1)); 
+//console.log(umaString.substring(umaString.length -5, umaString.length -1)); 
+console.log(umaString.toUpperCase()); 
+console.log(umaString.toLowerCase());

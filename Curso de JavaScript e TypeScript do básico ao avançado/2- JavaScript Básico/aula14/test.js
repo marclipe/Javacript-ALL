65bf937ee08d5e6eccf3bd2c0f2ca6f84375e9bf
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-12 14:49:47 -0300

    Math.

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/aula14/test.js
@@ -0,0 +1,18 @@
+let numeroUm = 9; 
+//console.log(numeroUm ** (1/2))
+//console.log(numeroUm ** 0.5); 
+
+
+//let numeroDois = Math.floor(numeroUm); 
+//let numeroDois = Math.ceil(numeroUm); 
+//let numeroDois = Math.round(numeroUm); 
+//console.log(Math.max(1,2,3,4,5,10,-50, 1500, 9, 8, 7, 6));
+//console.log(Math.min(1,2,3,4,5,10,-50, 1500, 9, 8, 7, 6));  
+
+//const numeroAletaorio = Math.round(Math.random() * (10 -5) + 5); 
+//console.log(Math.pow(2,10)); 
+//console.log(2 ** 10); 
+
+
+console.log(100 / 0); 
+

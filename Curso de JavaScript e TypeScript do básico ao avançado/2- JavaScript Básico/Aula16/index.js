50a0f01139910843cc502d06c240f633bb0fd618
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-04-01 22:40:06 -0300

    Arrays

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula16/index.js
@@ -0,0 +1,29 @@
+//                0       1       2
+let alunos = ['Luiz', 'Maria', 'João']; //Luiza, Eduardo 
+console.log(typeof alunos); 
+console.log(alunos instanceof Array); 
+
+//alunos.push('Luiza'); 
+//alunos.push('Eduardo'); 
+
+/*const removido = alunos.shift();
+console.log(removido); 
+console.log(alunos); */
+
+//alunos.unshift('Luiza'); //Adiciona no começo  
+//alunos.unshift('Fábio');  
+
+
+//alunos.push('Luiza'); //Adiciona no Fim 
+//alunos.push('Fábio'); 
+
+/*alunos[alunos.length] = 'Luiza'; //Adiciona no Fim 
+alunos[alunos.length] = 'Fábio';
+alunos[alunos.length] = 'Luana';*/ 
+
+//alunos[0] = 'Eduardo'; //Altera
+//alunos[3] = 'Luiza';  //Adicionando 
+
+/*console.log(alunos); 
+console.log(alunos[0]); 
+console.log(alunos[2]); */

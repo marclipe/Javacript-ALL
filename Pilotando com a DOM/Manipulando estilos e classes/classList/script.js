3449eeb47c6e7d8ba8243c6d54fb963c3fb383c5
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-04-08 22:45:14 -0300

    Mexendo no estilo

--- Pilotando com a DOM/Manipulando estilos e classes/classList/script.js
+++ Pilotando com a DOM/Manipulando estilos e classes/classList/script.js
@@ -2,6 +2,7 @@
 //classList 
 const element = document.querySelector('body'); 
 
-element.classList.add('active'); 
+element.classList.add('active', 'green'); 
 console.log(element.classList); 
-element.classList.remove('active')
+//element.classList.remove('active')
+element.classList.toggle('active')

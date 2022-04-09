d1cc231abb1bccfcf02f9a3e8c03cdeca39f2cf3
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-04-08 22:14:45 -0300

    Start with Node.js

---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula17/arrowFunction.js
@@ -0,0 +1,9 @@
+const raiz = (n) => {
+    return n ** 0.5; 
+};
+
+console.log(raiz(9)); 
+
+
+/*const raiz_ = (n) => n ** 0.5; 
+console.log(raiz_(16)); */
---
+++ Curso de JavaScript e TypeScript do básico ao avançado/2- JavaScript Básico/Aula17/index3.js
@@ -0,0 +1,5 @@
+const raiz = function (n) {
+    return n ** 0.5; 
+};
+
+console.log(raiz(9)); 

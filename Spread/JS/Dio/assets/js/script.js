828b74026403561d15feea71f277ad9d3abaf178
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-05-31 18:57:54 -0300

    Increment and Decrement

--- Spread/JS/Dio/assets/js/script.js
+++ Spread/JS/Dio/assets/js/script.js
@@ -1,12 +1,21 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0; 
+var currentNumber = 0;
+
+/* ============ Incrementar ============*/
+currentNumber.addEventListener('increment', increment())
 
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber; 
 }
 
+/* ============ Decrementar ============*/
+currentNumber.addEventListener('decrement', decrement())
+
 function decrement() {
     currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber; 
+    currentNumberWrapper.innerHTML = currentNumber;
 }
+
+/* ============ Condition ============*/
+

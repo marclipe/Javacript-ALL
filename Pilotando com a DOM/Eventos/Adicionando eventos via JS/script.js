e86e8c2fcf2ea8abbdfcda5f77c00f10a0d561b6
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-06-10 23:26:22 -0300

    Eventos DOM

---
+++ Pilotando com a DOM/Eventos/Adicionando eventos via JS/script.js
@@ -0,0 +1,9 @@
+//Events
+const h1 = document.querySelector('h1')
+
+h1.addEventListener('mouseover', print)
+/*h1.addEventListener('click', print)*/
+
+function print() {
+    console.log('print')
+}

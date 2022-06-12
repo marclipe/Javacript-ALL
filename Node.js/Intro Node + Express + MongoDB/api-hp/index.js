827a7c4b4ba6326510ffc7c418f03193f3e304f6
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-06-12 00:59:54 -0300

    Node + Express

--- Node.js/Intro Node + Express + MongoDB/api-hp/index.js
+++ Node.js/Intro Node + Express + MongoDB/api-hp/index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const characters = [
+  {
+    id: 1, 
+    name: "Harry Potter", 
+    species: "human", 
+    house: "Gryffinfor", 
+    actor: "Daniel Redcliffe"
+  }, 
+  {
+    id: 2, 
+    name: "Hermione Granger", 
+    species: "human", 
+    house: "Gryffinfor", 
+    actor: "Emma Watson"
+  }, 
+]
+
 //Rota 
-app.get('/', function (req, res) {
-  res.send('Home')
+//GET - READ
+app.get('/', (req, res) => {
+  res.send(characters)
 })
 
-function gett() {
-    console.log("olá mundo")
-}
+//POST - CREATE 
+app.post("/create", (req, res) => {
+  
+})
 
-gett()
-
-app.listen(3000, )
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000")
+});

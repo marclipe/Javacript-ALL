59ed754600896b2bb510c14bebacf181a09ef4c7
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-06-14 00:23:12 -0300

    CRUD ready!

--- Node.js/Intro Node + Express + MongoDB/api-hp/index.js
+++ Node.js/Intro Node + Express + MongoDB/api-hp/index.js
@@ -5,37 +5,86 @@ app.use(express.json())
 
 const characters = [
   {
-    id: 1, 
-    name: "Harry Potter", 
-    species: "human", 
-    house: "Gryffinfor", 
+    id: 1,
+    name: "Harry Potter",
+    spacies: "human",
+    house: "Gryffinfor",
     actor: "Daniel Redcliffe"
-  }, 
+  },
   {
-    id: 2, 
-    name: "Hermione Granger", 
-    species: "human", 
-    house: "Gryffinfor", 
+    id: 2,
+    name: "Hermione Granger",
+    spacies: "human",
+    house: "Gryffinfor",
     actor: "Emma Watson"
-  }, 
+  },
 ]
 
 //Rota 
 //GET - READ
 app.get('/', (req, res) => {
-  res.send(characters)
+  res.send(characters.filter(Boolean));
 })
 
 //POST - CREATE 
 app.post("/create", (req, res) => {
-  const character = req.body; 
+  const character = req.body;
 
   //Cria o ID
-  character.id = characters.length + 1; 
+  character.id = characters.length + 1;
   //Joga o ID dentro do Array de character
-  characters.push(character); 
+  characters.push(character);
 
-  res.send({message: "Personagem criado com sucesso!"})
+  res.send({ message: "Personagem criado com sucesso!" })
+})
+
+// getBYiD
+app.get("/character/:id", (req, res) => {
+  const id = +req.params.id;
+  const character = characters.find(c => c.id === id);
+
+  if (!character) {
+    res.status(404).send({ message: "Personagem não existe!" });
+    return;
+  }
+
+  res.send(character);
+})
+
+// PUT - UPDATE 
+app.put("/character/:id", (req, res) => {
+  const id = +req.params.id;
+  const character = characters.find((c) => c.id === id);
+
+  if (!character) {
+    res.status(404).send({ message: "Personagem não existe!" });
+    return;
+  }
+
+  const { name, spacies, house, actor } = req.body;
+
+  character.nome = name
+  character.spacies = spacies
+  character.house = house
+  character.actor = actor
+
+  res.send(character);
+});
+
+//DELETE - DELETE
+app.delete("/character/:id", (req, res) => {
+  const id = +req.params.id;
+  const character = characters.find(c => c.id === id);
+
+  if (!character) {
+    res.status(404).send({ message: "Personagem não existe!" });
+    return;
+  }
+
+  const indexCharacter = characters.indexOf(character); 
+  delete characters[indexCharacter]; 
+
+  res.send({message: "Personagem apagado com sucesso!"}); 
 })
 
 app.listen(3000, () => {

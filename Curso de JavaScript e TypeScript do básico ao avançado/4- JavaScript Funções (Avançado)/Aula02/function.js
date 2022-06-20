575561701a73c13a7955f3abd0201cf763e92f76
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-06-19 23:27:33 -0300

    Functions

--- Curso de JavaScript e TypeScript do básico ao avançado/4- JavaScript Funções (Avançado)/Aula02/function.js
+++ Curso de JavaScript e TypeScript do básico ao avançado/4- JavaScript Funções (Avançado)/Aula02/function.js
@@ -1,3 +1,12 @@
-function conta(operador, acumulador) {
-    
+function conta(operador, acumulador, ...numeros) {
+    for (let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '+') acumulador += numero;  
+        if (operador === '+') acumulador += numero;  
+        if (operador === '+') acumulador += numero;   
+    }
+
+    console.log(acumulador); 
 }
+
+conta('+', 0, 20, 30, 40, 50);

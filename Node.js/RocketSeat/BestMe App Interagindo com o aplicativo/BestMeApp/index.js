5d3d88489c7198dfd815b9347c07e644b3dbe75d
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-07-01 00:39:53 -0300

    process.on()

--- Node.js/RocketSeat/BestMe App Interagindo com o aplicativo/BestMeApp/index.js
+++ Node.js/RocketSeat/BestMe App Interagindo com o aplicativo/BestMeApp/index.js
@@ -1,12 +1,42 @@
 const questions = [
-    "o que aprendi hoje?", 
-    "o que me deixou aborrecido? E o que eu poderia fazer para melhorar?", 
+    "o que aprendi hoje?",
+    "o que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
     "O que me deixou feliz hoje?",
     "Quantas pessoas ajudei hoje?",
 ]
 
 const ask = (index = 0) => {
-    process.stdout.write(questions[index] + "\n\n\n"); 
+    process.stdout.write("\n\n" + questions[index] + " > ");
 }
 
 ask()
+
+const answers = []
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        //console.log(answers)
+        process.exit()
+    }
+})
+
+process.on('exit', () => {
+    console.log(`
+    Bacana Marcos! 
+
+    o que você aprendeu foi: 
+    ${answers[0]}
+
+    o que te aborreceu e você poderia melhorar foi: 
+    ${answers[1]}
+
+    O que te deixou feliz hoje: 
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!
+
+    Volte amanhã para novas reflexões..
+    `)
+})

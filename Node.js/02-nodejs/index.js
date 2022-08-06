8a0ec6bc2e719b47f6f11fb2e3572df2deb8b02e
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-08-05 21:42:39 -0300

    Node.js

--- Node.js/02-nodejs/index.js
+++ Node.js/02-nodejs/index.js
@@ -3,26 +3,63 @@
 2 Obter o endereço do usuário pelo Id 
 */
 
-function obterUsuario() {
+function obterUsuario(callback) {
     setTimeout(function () {
-        return {
+        return callback(null, {
             id: 1, 
             nome: 'Aladin', 
             dataNascimento: new Date()
-        }
+        })
     }, 1000)
 }
 
-function obterTelefone(idUsuario) {
-    
+function obterTelefone(idUsuario, callback) {
+    setTimeout(() => {
+        return callback(null, {
+            telefone: '1199002', 
+            ddd: 11
+        })
+    }, 2000); 
 }
 
-function obterEndereco(idUsuario) {
-    
+function obterEndereco(idUsuario, callback) {
+    setTimeout(() => {
+        return callback(null, {
+            rua: 'dos bobos', 
+            numero: 0
+        })
+    }, 2000)
 }
 
-const usuario = obterUsuario(); 
-const telefone = obterTelefone(usuario.id); 
+function resolverUsuario(erro, usuario) {
+    console.log('usuario', usuario);
+}
 
-console.log('usuario', usuario); 
-console.log('telefone', telefone); 
+obterUsuario(function resolverUsuario(error, usuario) {
+    //null || "" || 0 === false
+    if (error) {
+        console.log('DEU RUIM em USUARIO', error)
+        return; 
+    }
+    obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+        if (error1) {
+            console.log('DEU RUIM em TELEFONE', error)
+            return; 
+        }
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+            if (error2) {
+                console.log('DEU RUIM em ENDERECO', error)
+                return; 
+            }
+            console.log(`
+                Nome: ${usuario.nome}, 
+                Endereco: ${endereco.rua},${endereco.numero}. 
+                Telefon: (${telefone.ddd})${telefone.telefone}
+            `)
+        })
+    })
+}); 
+//const telefone = obterTelefone(usuario.id); 
+
+ 
+//console.log('telefone', telefone); 

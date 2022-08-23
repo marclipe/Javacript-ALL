f1196afe6e66358a274ff58d57cf83cd5b030520
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-08-23 20:16:51 -0300

    Funções

---
+++ Programacao_funcional/Higher-order/script.js
@@ -0,0 +1,19 @@
+//Exemplo com .map() JS
+const numbers = [2,4,8,16]
+
+const square = n => n * n //numero 
+
+const squareNumber = numbers.map(square)
+
+//Exemplo de um retorno de função 
+//(currying ou aplicação parcial de função)
+
+const pause = wait => fn => setTimeout(fn, wait)
+
+pause(600) (() => console.log('waiting 600ms'))
+
+const wait200 = pause(200)
+const wait1000 = pause(1000)
+
+wait200 (() => console.log('waiting 200ms'))
+wait1000 (() => console.log('waiting 1s'))

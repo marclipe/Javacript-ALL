59114db8d4d54400f15fec9ccc034b44c56667f4
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-10-07 00:56:17 -0300

    Function

---
+++ cataline/funcao.js
@@ -0,0 +1,12 @@
+function action() {
+    return 100
+}
+
+console.log(15 + action())
+
+/*passando parametros para a função*/
+function sum(number1, number2) {
+    return number1 + number2
+}
+
+console.log(sum(10, 50))

db46649c385174d5c60f573f8e50b7ae6747d588
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-10-08 22:40:07 -0300

    Array

---
+++ array_alura/array/listaLivros.js
@@ -0,0 +1,29 @@
+const precosLivros = [
+    {
+        titulo: "JavaScript",
+        preco: 25
+    },
+    {
+        titulo: "PHP", 
+        preco: 15
+    }, 
+    {
+        titulo: "JAVA", 
+        preco: 30
+    },
+    {
+        titulo: "Eixir", 
+        preco: 50
+    },
+    {
+        titulo: "Go",
+        preco: 45
+    }, 
+    {
+        titulo: "Python",
+        preco: 20 
+    }
+]; 
+
+module.exports = precosLivros; 
+
